61c5785e24c85f6157609ca9f810bc9b494ba8cc
Clifford64 <clifford641.jb@gmail.com>
2024-06-02 12:29:13 -0400

    Updated entrypoint for better error handling. Updated README

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -1,12 +1,40 @@
 #!/bin/sh
 
 echo "Starting JMusicBot by Jagrosh https://github.com/jagrosh/MusicBot"
-echo "Containerized by Craumix https://github.com/Craumix/jmb-container"
+echo "Containerized and updated by Clifford64 https://github.com/clifford64/MusicBot"
 echo "Version: $JMB_VERSION"
 
-cd /jmb/config
-java -jar -Dnogui=true /jmb/JMusicBot.jar
+# Check if config directory exists
+if [ ! -d "/jmb/config" ]; then
+    echo "Error: Config directory '/jmb/config' not found. Please ensure a volume or directory on the host system is mapped to /jmb/config"
+    exit 1
+fi
 
-echo "Seems like java stoped... Waiting for 30 seconds before termination..."
+# Check if config file exists
+if [ ! -f "/jmb/config/config.txt" ]; then
+    # Check if the user has write access to the config directory
+    if [ -w "/jmb/config" ]; then
+        echo "Downloading config.txt..."
+        curl -fsSL -o /jmb/config/config.txt https://github.com/jagrosh/MusicBot/raw/0.4.1/src/main/resources/reference.conf
+    else
+        echo "Error: '/jmb/config/config.txt' not found and run as user doesn't have write access to download the file automatically. Please update the permissions to the folder or download the file manually from 'https://github.com/jagrosh/MusicBot/raw/0.4.1/src/main/resources/reference.conf'" 
+        exit 1
+    fi
+fi
+
+# Check if the user has read access to the config file
+if [ ! -r "/jmb/config/config.txt" ]; then
+    echo "Error: Run as user doesn't have permissions to read '/jmb/config/config.txt'. Please update the permissions"
+    exit 1
+fi
+
+
+# Run the bot
+java -jar -Dconfig=/jmb/config/config.txt -Dnogui=true /jmb/JMusicBot.jar
+
+# Print stopping message and wait before termination
+echo "Seems like java stopped... Waiting for 30 seconds before termination..."
+echo ""
+echo "If you received a warning about 'No Token Provided', please check the config.txt file for the discord token for your server."
 sleep 30s
 echo "Terminating!"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Musicbot
 ### Upstream Repo - https://github.com/jagrosh/MusicBot/
 ##### This bot is built with Docker. I took the forked release and modified it so it no longer messages about new versions available until the original bot has a new verison available.
-##### I also modified the Docker container so it runs as a non-root user. The user has a UID of 3002 and matches a user on the host system.
+##### I also modified the Docker container so it runs as a non-root user.
 
 ##### Using Eclipse-Temurin base image. 
 
@@ -9,4 +9,18 @@
 
 ##### I have no idea what I am doing, so use at your own risk.
 
-`docker pull clifford64/musicbot:latest`
+## Setup
+
+### Run As User
+
+##### The default user specified in the Dockerfile has a UID of 3002. It is a good practice to specify a default non-root user for security purposes.
+
+##### You will have to ensure that the run as user has access to the folder and config file where it is hosted. Minimum permission required is read access and the config.txt already existing. If you grant the run as user write access, then it will automatically download the config.txt on first run. 
+
+##### You can specify a different user when running the container. Just change the "<UID-Run-As-User-Here>" in the example below
+
+`docker run --user=<UID-Run-As-User-Here> -v /path/to/config:/jmb/config clifford64/musicbot:latest`
+
+### First Run
+
+##### On first run, the container will attempt to download the config.txt file from the upstream repo if it doesn't already exist within the mapped folder. The container will then exit because the config.txt file will need to be updated with the necessary information to connect it to your discord server. Please look at the upstream repo setup instructions for more information. 
